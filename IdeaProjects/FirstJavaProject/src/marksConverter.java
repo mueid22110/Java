8994504fb8977f7988717e0bf55ea4afd89aecec
mueid22110 <mueidhasan9@gmail.com>
2024-10-22 12:05:19 +0600

    The percentage calculator is developed

--- IdeaProjects/FirstJavaProject/src/marksConverter.java
+++ IdeaProjects/FirstJavaProject/src/marksConverter.java
@@ -1,16 +1,33 @@
 import java.util.Scanner;
 
 public class marksConverter {
+
+    static float percent(int y, int x){
+
+        float result = (float) y *100 / x;
+
+        return result;
+    }
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of the subjects : ");
+        int sub = sc.nextInt();
 
-        System.out.print("Enter the total mark : ");
+        System.out.print("Enter the total mark per subject: ");
         int total = sc.nextInt();
+        float avg = 0.00f;
 
-        System.out.print("Enter the obtained mark : ");
-        int obt = sc.nextInt();
-        float result = (float) obt *100 / total;
+        for(int i = 1; i <= sub; i++){
 
-        System.out.println("The obtained mark is "+ result +"%\n");
+            System.out.print("Enter the mark of sub " + i + " : ");
+            int obt = sc.nextInt();
+            float result = percent(obt, total);
+            avg += result;
+        }
+        avg /= sub;
+
+
+        System.out.println("Your obtained mark is "+ avg +"%\n");
     }
 }
