2a2c3ae3fe8b447a91af5e654d8944fc9a2b2305
mueid22110 <mueidhasan9@gmail.com>
2024-11-12 05:14:42 +0600

    Result project using java

--- src/firstSemesterResult.java
+++ src/firstSemesterResult.java
@@ -37,11 +37,213 @@ public class firstSemesterResult {
         lamia.gpa= 3.428f;
         lamia.printDetails();
     }
+    static void siam(){
+        Student siam = new Student();
+        siam.id= 2338520101l;
+        siam.name= "Bakhtiar Islam Siam";
+        siam.gpa= 1.869f;
+        siam.position = 28;
+        siam.printDetails();
+    }
+    static void masuma(){
+        Student masuma = new Student();
+        masuma.id= 2338520102l;
+        masuma.name= "Masuma Sultana";
+        masuma.gpa= 1.940f;
+        masuma.position = 27;
+        masuma.printDetails();
+    }
+    static void manik(){
+        Student manik = new Student();
+        manik.id= 2338520103l;
+        manik.name= "Manik Barai";
+        manik.gpa= 2.869f;
+        manik.position = 10;
+        manik.printDetails();
+    }
+    static void rahee(){
+        Student rahee = new Student();
+        rahee.id= 2338520106l;
+        rahee.name= "Ridwana Rahee";
+        rahee.gpa= 2.595f;
+        rahee.position = 22;
+        rahee.printDetails();
+    }
+    static void naba(){
+        Student naba = new Student();
+        naba.id= 2338520107l;
+        naba.name= "Naba Chowdhury";
+        naba.gpa= 3.380f;
+        naba.position = 4;
+        naba.printDetails();
+    }
+    static void nishad(){
+        Student nishad = new Student();
+        nishad.id= 2338520109l;
+        nishad.name= "Naimur Rahman Nishad";
+        nishad.gpa= 3.250f;
+        nishad.position = 6;
+        nishad.printDetails();
+    }
+    static void jannat(){
+        Student jannat = new Student();
+        jannat.id= 2338520110l;
+        jannat.name= "Amily Jabin";
+        jannat.gpa= 3.297f;
+        jannat.position = 5;
+        jannat.printDetails();
+    }
+    static void mahfuz(){
+        Student mahfuz = new Student();
+        mahfuz.id= 2338520111l;
+        mahfuz.name= "Mahfuz Ahmed";
+        mahfuz.gpa= 2.678f;
+        mahfuz.position = 19;
+        mahfuz.printDetails();
+    }
+    static void muneem(){
+        Student muneem = new Student();
+        muneem.id= 2338520112l;
+        muneem.name= "Md. Muneem Alam";
+        muneem.gpa= 2.797f;
+        muneem.position = 13;
+        muneem.printDetails();
+    }
+    static void shefa(){
+        Student shefa = new Student();
+        shefa.id= 2338520113l;
+        shefa.name= "Sabah Tuz Shefa";
+        shefa.gpa= 2.619f;
+        shefa.position = 21;
+        shefa.printDetails();
+    }
+    static void tazul(){
+        Student tazul = new Student();
+        tazul.id= 2338520114l;
+        tazul.name= "Mohammad Tazul Islam";
+        tazul.gpa= 2.726f;
+        tazul.position = 16;
+        tazul.printDetails();
+    }
+    static void shaon(){
+        Student shaon = new Student();
+        shaon.id= 2338520115l;
+        shaon.name= "Md. Shaon Mollah";
+        shaon.gpa= 2.869f;
+        shaon.position = 11;
+        shaon.printDetails();
+    }
+    static void juney(){
+        Student juney = new Student();
+        juney.id= 2338520116l;
+        juney.name= "Sumshin Mosarrat";
+        juney.gpa= 1.083f;
+        juney.position = 29;
+        juney.printDetails();
+    }
+    static void shimul(){
+        Student shimul = new Student();
+        shimul.id= 2338520117l;
+        shimul.name= "Sd Imam Shimul";
+        shimul.gpa= 2.738f;
+        shimul.position = 17;
+        shimul.printDetails();
+    }
+    static void keya(){
+        Student keya = new Student();
+        keya.id= 2338520119l;
+        keya.name= "Most Keya Khatun";
+        keya.gpa= 3.226f;
+        keya.position = 7;
+        keya.printDetails();
+    }
+    static void alamin(){
+        Student alamin = new Student();
+        alamin.id= 2338520120l;
+        alamin.name= "Al-amin Safadi";
+        alamin.gpa= 2.869f;
+        alamin.position = 12;
+        alamin.printDetails();
+    }
+    static void shaharina(){
+        Student shaharina = new Student();
+        shaharina.id= 2338520121l;
+        shaharina.name= "Shaharina Khanom";
+        shaharina.gpa= 2.761f;
+        shaharina.position = 15;
+        shaharina.printDetails();
+    }
+    static void rashed(){
+        Student rashed = new Student();
+        rashed.id= 2338520122l;
+        rashed.name= "Md. Rasheduzzaman";
+        rashed.gpa= 3.071f;
+        rashed.position = 8;
+        rashed.printDetails();
+    }
+    static void dipa(){
+        Student dipa = new Student();
+        dipa.id= 2338520123l;
+        dipa.name= "Dipa Halder";
+        dipa.gpa= 3.035f;
+        dipa.position = 9;
+        dipa.printDetails();
+    }
+    static void siam2(){
+        Student siam2 = new Student();
+        siam2.id= 2338520124l;
+        siam2.name= "Md. Siam Sarder";
+        siam2.gpa= 2.702f;
+        siam2.position = 18;
+        siam2.printDetails();
+    }
+    static void tanha(){
+        Student tanha = new Student();
+        tanha.id= 2338520125l;
+        tanha.name= "Tanha Khan Abonty";
+        tanha.gpa= 2.190f;
+        tanha.position = 25;
+        tanha.printDetails();
+    }
+    static void fardul(){
+        Student fardul = new Student();
+        fardul.id= 2338520128l;
+        fardul.name= "Md. Fardul Islam";
+        fardul.gpa= 2.452f;
+        fardul.position = 23;
+        fardul.printDetails();
+    }
+    static void megh(){
+        Student megh = new Student();
+        megh.id= 2338520129l;
+        megh.name= "Habibur Rahman Shuvo";
+        megh.gpa= 2.285f;
+        megh.position = 24;
+        megh.printDetails();
+    }
+    static void fahim(){
+        Student fahim = new Student();
+        fahim.id= 2338520131l;
+        fahim.name= "Md. Fahim Hossen";
+        fahim.gpa= 2.678f;
+        fahim.position = 20;
+        fahim.printDetails();
+    }
+//    static void asad(){
+//        Student asad = new Student();
+//        asad.id= 2338520133l;
+//        asad.name= "MC Asad Hossain";
+//        asad.gpa= 0f;
+//        asad.position = 0;
+//        asad.printDetails();
+//    }
+
+
     static void Details() {
 
         Scanner sc = new Scanner(System.in);
         while (true){
-            System.out.println("Enter the roll Number off the student:");
+            System.out.println("Enter the last two digit of the roll Number off the student:");
             int roll = sc.nextInt();
             switch (roll) {
                 case 1:
@@ -59,13 +261,77 @@ public class firstSemesterResult {
                 case 6:
                     rahee();
                     break;
+                case 7:
+                    naba();
+                    break;
+                case 9:
+                    nishad();
+                    break;
+                case 10:
+                    jannat();
+                    break;
+                case 11:
+                    mahfuz();
+                    break;
+                case 12:
+                    muneem();
+                    break;
+                case 13:
+                    shefa();
+                    break;
+                case 14:
+                    tazul();
+                    break;
+                case 15:
+                    shaon();
+                    break;
+                case 16:
+                    juney();
+                    break;
+                case 17:
+                    shimul();
+                    break;
+                case 19:
+                    keya();
+                    break;
+                case 20:
+                    alamin();
+                    break;
+                case 21:
+                    shaharina();
+                    break;
+                case 22:
+                    rashed();
+                    break;
+                case 23:
+                    dipa();
+                    break;
+                case 24:
+                    siam2();
+                    break;
+                case 25:
+                    tanha();
+                    break;
                 case 27:
                     munem();
+                    break;
+                case 28:
+                    fardul();
+                    break;
+                case 29:
+                    megh();
                     break;
                 case 30:
                     lamia();
                     break;
+                case 31:
+                    fahim();
+                    break;
+//                case 33:
+//                    asad();
+//                    break;
                 default:
+                    System.out.println("Try again!!");
                     Details();
             }
         }
